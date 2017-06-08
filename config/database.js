be711a5f77be08898da774b1c5431a5f7c48a538
ing.joelstalinmartinez@gmail.com <JMartinez@IBCOMP15.IBSYSTEMS.COM>
2017-06-08 14:13:00 -0600

    -all

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    'url': 'mongodb://Admin:Admin1234@ds161400.mlab.com:61400/heroku_20t778v4' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+     
+     //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
+};
